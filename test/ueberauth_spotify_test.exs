6b01081d579543a1f17dfb417058dab8b9258950
Zoey Pessanha <zoey.spessanha@outlook.com>
2022-04-17 15:42:40 -0300

    feat: boilerplate test :sparkles:

--- test/ueberauth_spotify_test.exs
+++ test/ueberauth_spotify_test.exs
@@ -2,6 +2,8 @@ defmodule UeberauthSpotifyTest do
   use ExUnit.Case
   doctest UeberauthSpotify
 
+  @moduletag :unit
+
   test "greets the world" do
     assert UeberauthSpotify.hello() == :world
   end
